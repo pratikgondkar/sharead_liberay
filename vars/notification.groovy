930536601edced74dd6ec63785aed4c97f3fa6dc
Pratik Gondkar <131374322+pratikgondkar@users.noreply.github.com>
2024-08-30 23:23:59 +0530

    Update notification.groovy

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -1,6 +1,6 @@
 def call() {
     stage('notify') {
-          emailext body: 'success', subject: 'Status', to: 'prathamkukudkar@gmail.com'
-          slackSend channel: '#jenkins', message: 'successfull '
+          emailext body: 'success', subject: 'Status', to: 'pratikgondkar4040@gmail.com'
+          slackSend channel: '#jenkins-notification', message: 'successfull '
     }
 }
