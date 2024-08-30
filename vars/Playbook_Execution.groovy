538fe45b87d056e9daeebd71769c8b5f80d736eb
Pratik Gondkar <131374322+pratikgondkar@users.noreply.github.com>
2024-08-31 01:00:26 +0530

    Update Playbook_Execution.groovy

--- vars/Playbook_Execution.groovy
+++ vars/Playbook_Execution.groovy
@@ -1,7 +1,8 @@
 def call() {
     stage('playbook execution') {
         // Assuming the repository is in the Jenkins workspace
-        sh "ansible-playbook -i /home/pratik/prometheusrole/tests/inventory /home/pratik/prometheusrole/tests/test.yml"
+        sh "ansible-playbook -i ${WORKSPACE}/prometheusrole/tests/inventory ${WORKSPACE}/prometheusrole/tests/test.yml"
+
     }
 }
 
