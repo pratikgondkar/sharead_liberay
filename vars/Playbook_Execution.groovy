5179912975712dff784af7c671f3398286053f43
Pratik Gondkar <131374322+pratikgondkar@users.noreply.github.com>
2024-08-30 23:11:37 +0530

    Update Playbook_Execution.groovy

--- vars/Playbook_Execution.groovy
+++ vars/Playbook_Execution.groovy
@@ -1,6 +1,6 @@
 def call() {
     stage('playbook execution') {
-        sh "ansible-playbook -i inventory test.yml"
+        sh "ansible-playbook -i inventory /home/pratik/assignment/Ansible/Assignment05/prometheusrole/tests/test.yml"
     }
 }
 
