8628d7443ea01062ff9abc44fd263e0a9b0ede07
Pratik Gondkar <131374322+pratikgondkar@users.noreply.github.com>
2024-08-30 23:22:51 +0530

    Update ansible_check.groovy

--- vars/ansible_check.groovy
+++ vars/ansible_check.groovy
@@ -1,5 +1,5 @@
 def call() {
     stage('Clone Repository') {
-        git branch: 'main', url: 'https://github.com/PRATHAMPRAJU/Docker_role.git'
+        git branch: 'main', url: 'https://github.com/pratikgondkar/-Prometheus-_role.git'
     }
 }
